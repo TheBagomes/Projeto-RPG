d7202af9940a08c2667d9d8ce9ddfb6fe821b2a1
OBagomes <vitorbarretogomes1@gmail.com>
2025-11-03 18:30:34 -0300

    algumas mudanças

--- menu_principal.py
+++ menu_principal.py
@@ -4,19 +4,13 @@ def mostrar_menu_principal():
     while True:
         print("\n=== Projeto RPG ===")
         print("1) Iniciar Jogo")
-        print("2) Carregar Jogo")
-        print("3) Opções")
-        print("4) Sair")
+        print("2) Sair")
 
         escolha = input("Escolha uma opção: ")
 
         if escolha == "1":
             iniciar_jogo()
         elif escolha == "2":
-            carregar_jogo()
-        elif escolha == "3":
-            opcoes()
-        elif escolha == "4":
             print("Saindo do jogo...")
             break
         else:
@@ -25,11 +19,5 @@ def mostrar_menu_principal():
 def iniciar_jogo():
      print("\nIniciando novo jogo...\n")
      menu.menu()  
-
-def carregar_jogo():
-    print("\nFunção de carregar jogo ainda não implementada.\n")
-
-def opcoes():
-    print("\nConfigurações:\n(futuramente: som, dificuldade, idioma...)\n")
 
 
